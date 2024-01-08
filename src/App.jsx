86be91b91818ff97f24bd09f69f262d27a034262
shivakumar <skchintu000@gmail.com>
2024-01-08 12:12:10 +0530

    after build

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ function App() {
          <h1 className="text-2xl font-bold text-center"> hello world</h1>
          <h2>hii how are you</h2>
          <h3 className="text-4xl">iam fine</h3>
+         <h4 className="text-5xl font-bold">after npm run build</h4>
       </>
    );
 }
